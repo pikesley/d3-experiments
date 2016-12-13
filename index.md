c2c198795339757c24e863bb8f3342d7482491b2
pikesley <github@orgraphone.org>
2016-12-13 21:11:10 +0000

    Better menus

--- index.md
+++ index.md
@@ -1,4 +1,10 @@
 ---
 ---
 
-# Experiments with [D3](https://d3js.org/)
+<h1>Experiments with <a href='https://d3js.org/'>D3</a></h1>
+
+<ul id='main-list'>
+  <li><a href='gradients/'>Gradients</a></li>
+  <li><a href='monte-carlo/'>Approximating π with Monte Carlo methods</a></li>
+  <li><a href='clock/'>Clock</a></li>
+</ul>
--- js/gradients.js
+++ js/gradients.js
@@ -2,8 +2,45 @@
 layout: null
 ---
 
+interpolaters = [
+  d3.interpolateRgb,
+  d3.interpolateHsl,
+  d3.interpolateHslLong,
+  d3.interpolateLab,
+  d3.interpolateHcl
+]
+
 d3.json('{{ site.baseurl }}/assets/images/logos/sam.pikesley.org.svg.json', function(data) {
-  weighting = d3.scaleLinear()
+  rgb = d3.scaleLinear()
+                .domain([0, data.width])
+                .interpolate(d3.interpolateRgb)
+                .range([
+                  d3.rgb(getFillColour('.logo .pixel')),
+                  d3.rgb(getFillColour('.logo .alt-pixel'))
+                ])
+
+  render('#interpolated-rgb')
+      .classed('pixel', false)
+      .attr('fill', function(d) {
+        return rgb(d.x)
+      })
+
+  hsl = d3.scaleLinear()
+                .domain([0, data.width])
+                .interpolate(d3.interpolateHsl)
+                .range([
+                  d3.rgb(getFillColour('.logo .pixel')),
+                  d3.rgb(getFillColour('.logo .alt-pixel'))
+                ])
+
+  render('#interpolated-hsl')
+      .classed('pixel', false)
+      .attr('fill', function(d) {
+        return hsl(d.x)
+      })
+
+
+  hcl = d3.scaleLinear()
                 .domain([0, data.width])
                 .interpolate(d3.interpolateHcl)
                 .range([
@@ -11,34 +48,36 @@ d3.json('{{ site.baseurl }}/assets/images/logos/sam.pikesley.org.svg.json', func
                   d3.rgb(getFillColour('.logo .alt-pixel'))
                 ])
 
-  i = render('interpolated-hcl')
+  render('#interpolated-hcl')
       .classed('pixel', false)
       .attr('fill', function(d) {
-        return weighting(d.x)
+        return hcl(d.x)
       })
 
-  weighting = d3.scaleLinear()
+  lab = d3.scaleLinear()
                 .domain([0, data.width])
-                .interpolate(d3.interpolateLab)
+                .interpolate(d3.interpolateHcl)
                 .range([
                   d3.rgb(getFillColour('.logo .pixel')),
                   d3.rgb(getFillColour('.logo .alt-pixel'))
                 ])
 
-  i = render('interpolated-lab')
+  render('#interpolated-lab')
       .classed('pixel', false)
       .attr('fill', function(d) {
-        return weighting(d.x)
+        return lab(d.x)
       })
+
+
 
   // linear
   weighting = d3.scaleLinear()
                 .domain([0, data.width])
                 .range([0, 1])
-  twoTone('linear', function(x) {
+  twoTone('#linear', function(x) {
     return (0.5 > weighting(x))
   })
-  twoTone('linear-fuzzed', function(x) {
+  twoTone('#linear-fuzzed', function(x) {
     return (Math.random() > weighting(x))
   })
 
@@ -46,10 +85,10 @@ d3.json('{{ site.baseurl }}/assets/images/logos/sam.pikesley.org.svg.json', func
   weighting = d3.scaleLinear()
                 .domain([0, data.width])
                 .range([Math.PI, 0])
-  twoTone('half-cos', function(x) {
+  twoTone('#half-cos', function(x) {
     return (0.5 > ((Math.cos(weighting(x)) + 1) / 2))
   })
-  twoTone('half-cos-fuzzed', function(x) {
+  twoTone('#half-cos-fuzzed', function(x) {
     return (Math.random() > ((Math.cos(weighting(x)) + 1) / 2))
   })
 
@@ -57,10 +96,10 @@ d3.json('{{ site.baseurl }}/assets/images/logos/sam.pikesley.org.svg.json', func
   weighting = d3.scaleLinear()
                 .domain([0, data.width])
                 .range([-Math.PI, Math.PI])
-  twoTone('cos', function(x) {
+  twoTone('#cos', function(x) {
     return (0.5 > ((Math.cos(weighting(x)) + 1) / 2))
   })
-  twoTone('cos-fuzzed', function(x) {
+  twoTone('#cos-fuzzed', function(x) {
     return (Math.random() > ((Math.cos(weighting(x)) + 1) / 2))
   })
 
@@ -77,7 +116,7 @@ d3.json('{{ site.baseurl }}/assets/images/logos/sam.pikesley.org.svg.json', func
 
   for (i = 0; i < mcs.length; i++) {
     name = mcs[i]
-    mc = render('multicolour-' + name)
+    mc = render('#multicolour-' + name)
     for (j = 0; j < 5; j++) {
       mc.classed(name + '-shade-' + j, function(d) {
         if (weighting(d.x) == j) {
@@ -90,7 +129,7 @@ d3.json('{{ site.baseurl }}/assets/images/logos/sam.pikesley.org.svg.json', func
   }
 
   function base(div) {
-    return d3.select('#' + div)
+    return d3.select(div)
              .append('div')
              .classed('svg-container', true)
              .append('svg')
@@ -128,7 +167,7 @@ d3.json('{{ site.baseurl }}/assets/images/logos/sam.pikesley.org.svg.json', func
 
 // http://stackoverflow.com/questions/16965515/how-to-get-a-style-attribute-from-a-css-class-by-javascript-jquery/16965902#16965902
   function getFillColour(selector) {
-    rules = document.styleSheets[2].cssRules
+    rules = document.styleSheets[3].cssRules
     for(i in rules) {
       if(rules[i].selectorText == selector) return rules[i].style.fill
     }
